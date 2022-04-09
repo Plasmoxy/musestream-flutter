96849e52d6387c878bf0eb21bdce7f18df7765b5
Plasmoxy <plasmoxy@gmail.com>
2022-04-09 17:48:22 +0200

    screens setup

---
+++ musestream_app/lib/screens/debug.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:musestream_app/screens/login.dart';
+
+class DebugScreen extends StatefulWidget {
+  DebugScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DebugScreen> createState() => _DebugScreenState();
+}
+
+class _DebugScreenState extends State<DebugScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold a Appbar pre kazdru screenu
+    // SingleChildScrollView -> Column
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Debug screen'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: Text('to login screen'),
+              onPressed: () {
+                // navigation to different screen
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (ctx) => LoginScreen(),
+                ));
+              },
+            ),
+
+            // karta + padding a margin
+            Card(
+              margin: EdgeInsets.all(16),
+              // REFACTOR MENU = Ctrl Shift R
+              child: InkWell(
+                onTap: () {
+                  // navigation to different screen
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (ctx) => LoginScreen(),
+                  ));
+                },
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(16),
+                  // co je vnutri tej karty ?
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start, // for esample
+                          children: [
+                            Text(
+                              'Lesson name',
+                              style: TextStyle(fontSize: 30), // for example
+                            ),
+                            Text('Teacher name'),
+                          ],
+                        ),
+                      ),
+                      Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                        width: 40,
+                        height: 40,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              child: Text('ahpoj'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
