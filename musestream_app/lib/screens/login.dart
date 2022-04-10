555ebe5a89e1d612358e510714a5e04d06060b83
ottonovak <xnovaks1@stuba.sk>
2022-04-10 11:14:39 +0200

    Login screen

--- musestream_app/lib/screens/login.dart
+++ musestream_app/lib/screens/login.dart
@@ -10,8 +10,58 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('ahoj'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Login"),
+        ),
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(14),
+                decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.grey))),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Email",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(14),
+                decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.grey))),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.all(10),
+                  child: FlatButton(
+                    child: Text(
+                      'LogIn',
+                    ),
+                    color: Colors.blueAccent,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                  )),
+              Container(
+                  margin: EdgeInsets.all(5),
+                  child: FlatButton(
+                    child: Text(
+                      'Register',
+                    ),
+                    color: Colors.blueAccent,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                  ))
+            ],
+          ),
+        ));
   }
 }
